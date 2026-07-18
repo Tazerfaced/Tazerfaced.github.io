0e506dcd8bd6ea42959e04bf68037cba9a1824c3
Tazerfaced <68751289+Tazerfaced@users.noreply.github.com>
2026-07-18 13:05:26 -0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Discovery Pediatric Dentistry Website Scripts
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("Discovery Pediatric Dentistry website loaded.");
+
+});
